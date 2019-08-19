b4bd7d341d5c4a6674485fca1bcb8da1a928a8c0
elianatorres5772 <42104414+elianatorres5772@users.noreply.github.com>
2019-08-19 13:50:52 -0500

    se realiza el editar

--- MyLeasing/MyLeasing/Helpers/IConverterHelper.cs
+++ MyLeasing/MyLeasing/Helpers/IConverterHelper.cs
@@ -6,6 +6,7 @@ namespace MyLeasing.Web.Helpers
 {
     public interface IConverterHelper
     {
-        Task<Property> ToPropertyAsync(PropertyViewModel model, bool isnew);
+        Task<Property> ToPropertyAsync(PropertyViewModel model, bool isNew);
+        PropertyViewModel ToPropertyViewModel(Property property);
     }
 }
