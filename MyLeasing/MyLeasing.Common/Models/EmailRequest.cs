5cce36129c8fadf0d08c168eb28ed151475a1f61
elianatorres5772 <42104414+elianatorres5772@users.noreply.github.com>
2019-09-08 16:21:22 -0500

    error

---
+++ MyLeasing/MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyLeasing.Common.Models
+{
+    class EmailRequest
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
