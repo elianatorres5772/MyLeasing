d56a0227c172d517affbb02a7c37f03a61811a15
elianatorres5772 <42104414+elianatorres5772@users.noreply.github.com>
2019-09-08 17:58:27 -0500

    se termina y compila video 18

--- MyLeasing/MyLeasing/Controllers/API/OwnersController.cs
+++ MyLeasing/MyLeasing/Controllers/API/OwnersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Common.Models;
 using MyLeasing.Web.Data;
+using MyLeasing.Web.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,28 +23,93 @@ namespace MyLeasing.Web.Controllers.API
             _dataContext = dataContext;
         }
 
+
         [HttpPost]
-        [Route("GetOnwerByEmail")] //llama el usrio 
-        public  async Task<IActionResult> GetOnwerByEmailASync( EmailRequest Request)
+        [Route("GetOnwerByEmail")] 
+        
 
 
+        public async Task<IActionResult> GetOwnerByEmailAsync(EmailRequest request)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
-            var owner = await _dataContext.Owners
-                
-           .FirstOrDefaultAsync(o => o.User.Email.ToLower() == Request.Email.ToLower());
+            // estamos incluyendo las talas 
 
+
+            var owner = await _dataContext.Owners
+                .Include(o => o.User)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyType)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyImages)
+                .Include(o => o.Contracts)
+                .ThenInclude(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .FirstOrDefaultAsync(o => o.User.Email.ToLower() == request.Email.ToLower());
 
             if (owner == null)
-
             {
                 return NotFound();
-
             }
-            return Ok(owner);
+
+            var response = new OwnerResponse
+            {
+                Id = owner.Id,
+                FirstName = owner.User.FirstName,
+                LastName = owner.User.LastName,
+                Address = owner.User.Address,
+                Document = owner.User.Document,
+                Email = owner.User.Email,
+                PhoneNumber = owner.User.PhoneNumber,
+                Properties = owner.Properties?.Select(p => new PropertyResponse // este  signo de ig
+                {
+                    Address = p.Address,
+                    Contracts = p.Contracts?.Select(c => new ContractResponse
+                    {
+                        EndDate = c.EndDate,
+                        Id = c.Id,
+                        IsActive = c.IsActive,
+                        Lessee = ToLessesResponse(c.Lessee),
+                        Price = c.Price,
+                        Remarks = c.Remarks,
+                        StartDate = c.StartDate
+                    }).ToList(),
+                    HasParkingLot = p.HasParkingLot,
+                    Id = p.Id,
+                    IsAvailable = p.IsAvailable,
+                    Neighborhood = p.Neighborhood,
+                    Price = p.Price,
+                    PropertyImages = p.PropertyImages?.Select(pi => new PropertyImageResponse
+                    {
+                        Id = pi.Id,
+                        ImageUrl = pi.ImageFullPath
+                    }).ToList(),
+                    PropertyType = p.PropertyType.Name,
+                    Remarks = p.Remarks,
+                    Rooms = p.Rooms,
+                    SquareMeters = p.SquareMeters,
+                    Stratum = p.Stratum
+                }).ToList()
+            };
+
+            return Ok(response);
         }
+
+        private LesseeResponse ToLessesResponse(Lessee lessee) // se esta importando una clase 
+        {
+            return new LesseeResponse
+            {
+                Id = lessee.Id,
+                Address = lessee.User.Address,
+                Document = lessee.User.Document,
+                Email = lessee.User.Email,
+                FirstName = lessee.User.FirstName,
+                LastName = lessee.User.LastName,
+                PhoneNumber = lessee.User.PhoneNumber
+            };
+        }
+
     }
 }
