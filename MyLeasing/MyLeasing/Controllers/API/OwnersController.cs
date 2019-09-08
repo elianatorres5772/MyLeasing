16a9a54df12dca73d8636443a5e73eeaaf8c62da
elianatorres5772 <42104414+elianatorres5772@users.noreply.github.com>
2019-09-08 17:28:36 -0500

    se termina video 23

---
+++ MyLeasing/MyLeasing/Controllers/API/OwnersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyLeasing.Common.Models;
+using MyLeasing.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Controllers.API
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public OwnersController (DataContext dataContext )
+
+            {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        [Route("GetOnwerByEmail")] //llama el usrio 
+        public  async Task<IActionResult> GetOnwerByEmailASync( EmailRequest Request)
+
+
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var owner = await _dataContext.Owners
+                
+           .FirstOrDefaultAsync(o => o.User.Email.ToLower() == Request.Email.ToLower());
+
+
+            if (owner == null)
+
+            {
+                return NotFound();
+
+            }
+            return Ok(owner);
+        }
+    }
+}
